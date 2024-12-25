0da3391fe43f2f45c99bc94f14189cb4fd898982
LuK1337 <priv.luk@gmail.com>
2024-12-25 23:42:18 +0100

    sm8650-common: Switch to NCM USB tethering
    Change-Id: I20b6b2b2e26a45d8b28c7483afc83ad4da4b8643

--- common.mk
+++ common.mk
@@ -189,6 +189,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     CarrierConfigResCommon_Sys \
     FrameworksResTarget_Vendor \
+    NcmTetheringOverlay \
     SystemUIResCommon_Sys \
     TelephonyResCommon_Sys \
     WifiResCommon_Sys \
