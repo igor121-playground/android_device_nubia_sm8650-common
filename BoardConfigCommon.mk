97e076a0c348ae77f7d661a366c690a0e27f56c5
igor121 <chernokalsky89@gmail.com>
2025-07-08 10:35:22 +0300

    sm8650: fixup d467b3d29a812e3a5df742460ff48cf03a520968
    * we're not caza anymore

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -63,7 +63,7 @@ TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
 
 # Fingerprint
 SOONG_CONFIG_qtidisplay_udfps := true
-TARGET_SURFACEFLINGER_UDFPS_LIB := //$(DEVICE_PATH):libudfps_extension.caza
+TARGET_SURFACEFLINGER_UDFPS_LIB := //$(COMMON_PATH):libudfps_extension.sm8650
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
