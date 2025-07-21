eeb19c97f56eb37069f7719196d5ad782151815d
John Galt <johngaltfirstrun@gmail.com>
2025-07-21 19:38:26 +0300

    sm8650-common: utilize task_profiles.json + cgroups.json
    We have full kernel support for uclamp and all additional functionality
    provided on current profiles.

--- common.mk
+++ common.mk
@@ -310,6 +310,11 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/qcom-caf/common/libqti-perfd-client \
     vendor/qcom/opensource/usb/etc
 
+# Task Profiles
+PRODUCT_COPY_FILES += \
+    system/core/libprocessgroup/profiles/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json \
+    system/core/libprocessgroup/profiles/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json
+
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal-service.qti
