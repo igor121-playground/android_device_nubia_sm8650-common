6072566a2b8400a6802249cf9971fe2617102335
Dhina17 <dhinalogu@gmail.com>
2025-07-28 17:38:56 -0400

    sm8650-common: implement torch light brightness control

--- common.mk
+++ common.mk
@@ -102,6 +102,7 @@ TARGET_ENABLE_BLUR := true
 
 # Camera
 $(call soong_config_set,camera,override_format_from_reserved,true)
+$(call soong_config_set,libcameraservice,ext_lib,//$(COMMON_PATH):libcameraservice_extension.nubia_sm8650)
 
 # Display
 PRODUCT_PACKAGES += \
