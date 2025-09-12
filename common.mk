1878a799df06f7de740ad312c361c9c8ae39583f
Giovanni Ricca <giovanniricca@proton.me>
2025-09-12 23:52:39 +0200

    Revert "sm8650-common: decommonize display configs"
    They are shared across both tiro and cerro.
    
    This reverts commit 06a94162d045f2a49c43c12256e3676ad881ee28.
    
    Change-Id: I849beba0cf963958a813b8c0aa874b914869111b

--- common.mk
+++ common.mk
@@ -125,6 +125,9 @@ PRODUCT_PACKAGES += \
     init.qti.display_boot.sh \
     libgpu_tonemapper
 
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/configs/display_id_4630946566460097411.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630946566460097411.xml \
+    $(COMMON_PATH)/configs/display_id_4630947039571902851.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630947039571902851.xml
 
 # DisplayFeatures
 PRODUCT_PACKAGES += \
