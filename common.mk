4a9e754a4ea8a395c9f1a3012e69e995b6147b66
Igor121 <chernokalsky89@gmail.com>
2025-10-12 18:54:37 +0500

    Revert "sm8650-common: begin building with Lindroid"
    This reverts commit 2dad17e84ef227af4cd89e0ba23aad9e1d0862bc.

--- common.mk
+++ common.mk
@@ -258,10 +258,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2023-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
 
-
-# Lindroid
-TARGET_BUILD_LINDROID := true
-
 # Platform
 TARGET_BOARD_PLATFORM := pineapple
 
