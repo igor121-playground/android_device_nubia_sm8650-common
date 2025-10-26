35dcfe2c41b42375f1aa5511b5edbe84dd18ac32
Igor121 <chernokalsky89@gmail.com>
2025-10-26 19:38:14 +0500

    Revert "sm8650-common: Enable ELF checks for audio blobs"
    This reverts commit b21b378625e329bb989bd31c3907a411f9e4585a.

--- extract-files.py
+++ extract-files.py
@@ -9,6 +9,7 @@ from extract_utils.fixups_blob import (
     blob_fixups_user_type,
 )
 from extract_utils.fixups_lib import (
+    lib_fixup_remove,
     lib_fixups,
     lib_fixups_user_type,
 )
@@ -52,6 +53,14 @@ lib_fixups: lib_fixups_user_type = {
         'vendor.qti.qccvndhal_aidl-V1-ndk',
         'libskia',
     ): lib_fixup_vendor_suffix,
+    (
+        'libar-pal',
+        'libar-acdb',
+        'liblx-osal',
+        'libats',
+        'libagm',
+        'libpalclient',
+    ): lib_fixup_remove,
 }
 
 blob_fixups: blob_fixups_user_type = {
